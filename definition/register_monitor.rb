a68612628fa2d012d2e6e48a860a5bfff54bbd36
Curt Wilhelm <curt@rightscale.com>
2014-07-09 16:41:44 -0600

    create a definition for creating the monitor

---
+++ definition/register_monitor.rb
@@ -0,0 +1,48 @@
+
+define :register_slave, :action=> :create do
+  # Obtain information about nat ha servers by querying for its tags
+
+      r = rightscale_server_collection "nat_ha" do
+        timeout 3600 #1hr
+        tags "nat:ha=enabled"
+        mandatory_tags "nat:ha=enabled"
+        empty_ok false
+        action :nothing
+      end
+      r.run_action(:load)
+  
+  nat_server_id = ""
+  nat_server_ip = ""
+
+  f = ruby_block "find tags from servers" do
+    block do
+      node[:server_collection]["nat_ha"].each do |id, tags|
+        server_ip_tag = tags.detect { |u| u =~ /server:private_ip_0/ }
+        nat_server_ip = server_ip_tag.split(/=/, 2).last.chomp
+        server_id_tag = tags.detect { |u| u =~ /nat:server_id/ }
+        nat_server_id = server_id_tag.split(/=/, 2).last.chomp
+
+        Chef::Log.info "Nat Server ID: #{nat_server_id}"
+        Chef::Log.info "Nat Server IP: #{nat_server_ip}"
+      end
+    end
+  end
+
+  f.run_action(:create)
+  
+  
+  template "/root/nat-monitor.sh" do
+    source "nat-monitor.erb"
+    owner  "root"
+    group  "root"
+    mode   "0700"
+    variables( :other_instance_id=> nat_server_id,
+      :my_instance_id=> node[:ec2][:instance_id],
+      :other_instance_ip=>  nat_server_ip,
+      :ec2_url => node[:aws][:vpc_nat][:ec2_url],
+      :java_home => node[:aws][:vpc_nat][:java_home]
+    )
+    action :create
+  end
+  
+end
--- recipes/vpc-nat-ha.rb
+++ recipes/vpc-nat-ha.rb
@@ -21,54 +21,10 @@ if node[:aws][:vpc_nat][:nat_ha]=='enabled'
   
   log "NAT HA enabled.  Proceding to install nat-monitor"  
   
-  # Obtain information about nat ha servers by querying for its tags
-  servers = ruby_block "find ha servers" do
-    block do
-      r = rightscale_server_collection "nat_ha" do
-        timeout 3600 #1hr
-        tags "nat:ha=enabled"
-        mandatory_tags "nat:ha=enabled"
-        empty_ok false
-        action :nothing
-      end
-      r.run_action(:load)
-    end
-  end
-  servers.run_action(:create)
-  
-  nat_server_id = ""
-  nat_server_ip = ""
-
-  f = ruby_block "find tags from servers" do
-    block do
-      node[:server_collection]["nat_ha"].each do |id, tags|
-        server_ip_tag = tags.detect { |u| u =~ /server:private_ip_0/ }
-        nat_server_ip = server_ip_tag.split(/=/, 2).last.chomp
-        server_id_tag = tags.detect { |u| u =~ /nat:server_id/ }
-        nat_server_id = server_id_tag.split(/=/, 2).last.chomp
-
-        Chef::Log.info "Nat Server ID: #{nat_server_id}"
-        Chef::Log.info "Nat Server IP: #{nat_server_ip}"
-      end
-    end
-  end
-
-  f.run_action(:create)
-  
-  
-  template "/root/nat-monitor.sh" do
-    source "nat-monitor.erb"
-    owner  "root"
-    group  "root"
-    mode   "0700"
-    variables( :other_instance_id=> nat_server_id,
-      :my_instance_id=> node[:ec2][:instance_id],
-      :other_instance_ip=>  nat_server_ip,
-      :ec2_url => node[:aws][:vpc_nat][:ec2_url],
-      :java_home => node[:aws][:vpc_nat][:java_home]
-    )
+  register_monitor "Register NAT HA Montior" do
     action :create
   end
+  
   template "/root/.awscreds" do
     source "credentials.erb"
     owner  "root"
