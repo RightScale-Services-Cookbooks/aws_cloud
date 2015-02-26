7cf23d37337cdc7d9d31c80710807a8f9cfdeaac
Richard Shade <rshade@rightscale.com>
2015-02-26 17:21:34 -0600

    removing aws from region

--- providers/lb.rb
+++ providers/lb.rb
@@ -6,7 +6,7 @@ action :connect do
   elb=Fog::AWS::ELB.new(
       :aws_access_key_id => node[:aws][:aws_access_key_id],
       :aws_secret_access_key => node[:aws][:aws_secret_access_key],
-      :aws_region  => node[:aws][:region] )
+      :region  => node[:aws][:region] )
   elb.register_instances_with_load_balancer([node_id],elb_name)
   new_resource.updated_by_last_action(true)
 end
@@ -18,7 +18,7 @@ action :disconnect do
   elb=Fog::AWS::ELB.new(
       :aws_access_key_id => node[:aws][:aws_access_key_id],
       :aws_secret_access_key => node[:aws][:aws_secret_access_key],
-      :aws_region  => node[:aws][:region] )
+      :region => node[:aws][:region] )
   elb.deregister_instances_from_load_balancer([node_id],node_id)
   new_resource.updated_by_last_action(true)
 end
