0919b6022744b4ecebf420915b73d9c41934bf01
Curt Wilhelm <curt@rightscale.com>
2015-11-17 13:22:45 -0700

    test only fog install

--- test/cookbooks/fake/recipes/lb_connect.rb
+++ test/cookbooks/fake/recipes/lb_connect.rb
@@ -3,16 +3,3 @@
 
 include_recipe "fog::default"
 
-#chef_gem 'fog'
-require 'fog'
-require "fog/aws"
-#r= ruby_block 'fog mock' do
-#  block do
-    Fog.mock!
-#  end
-#  action :nothing
-#end
-
- #r.run_action(:run)
-
-include_recipe "aws::lb_connect"
